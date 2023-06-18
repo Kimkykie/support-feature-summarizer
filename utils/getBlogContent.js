0b3ebff2acbc575d4fcde647cc4f3a6f182a38af
Kim Kiragu <Kimkiragu@gmail.com>
2023-06-18 14:20:38 +0300

    fix: Add chromium-aws to fix puppeteer failing on vercel

--- utils/getBlogContent.js
+++ utils/getBlogContent.js
@@ -1,4 +1,15 @@
-const puppeteer = require("puppeteer");
+// api/run.js
+import edgeChromium from 'chrome-aws-lambda'
+
+// Importing Puppeteer core as default otherwise
+// it won't function correctly with "launch()"
+import puppeteer from 'puppeteer-core'
+
+// You may want to change this if you're developing
+// on a platform different from macOS.
+// See https://github.com/vercel/og-image for a more resilient
+// system-agnostic options for Puppeteeer.
+const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 const SummarizerManager = require("node-summarizer").SummarizerManager;
 
 async function getBlogContent(url) {
@@ -6,7 +17,13 @@ async function getBlogContent(url) {
   let content;
 
   try {
-    browser = await puppeteer.launch({ headless: true });
+    // Edge executable will return an empty string locally.
+    const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
+    const browser = await puppeteer.launch({
+      executablePath,
+      args: edgeChromium.args,
+      headless: false,
+    });
     const page = await browser.newPage();
 
     await page.setExtraHTTPHeaders({
